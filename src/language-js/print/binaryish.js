bef9c689d4e212fa484d687bf1cb7c6f026f4774
Brandon Mills <mills.brandont@gmail.com>
2018-11-02 00:11:02 -0400

    Break lines before binary operators

--- src/language-js/print/binaryish.js
+++ src/language-js/print/binaryish.js
@@ -231,9 +231,6 @@ function printBinaryishExpressions(
 
   const shouldInline = shouldInlineLogicalExpression(node);
   const lineBeforeOperator =
-    (node.operator === "|>" ||
-      node.type === "NGPipeExpression" ||
-      (node.operator === "|" && options.parser === "__vue_expression")) &&
     !hasLeadingOwnLineComment(options.originalText, node.right);
 
   const operator = node.type === "NGPipeExpression" ? "|" : node.operator;
@@ -294,7 +291,7 @@ function printBinaryishExpressions(
       node.right.type !== node.type);
 
   parts.push(
-    lineBeforeOperator ? "" : " ",
+    lineBeforeOperator && !shouldInline ? "" : " ",
     shouldGroup ? group(right, { shouldBreak }) : right
   );
 
